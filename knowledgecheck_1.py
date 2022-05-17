840b9acefb4ffcf0ae1f7a6a5e1e841fca470c61
m-h-rodriguez <105326343+m-h-rodriguez@users.noreply.github.com>
2022-05-17 19:48:14 -0400

    Testing commit git

---
+++ knowledgecheck_1.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+
+test_list = ['Michelle', 'Code Louisville', 'Learning']
+
+print('My name is', test_list[0])
+
+fav_dict={
+'books': ['Harry Potter', 'Jurassic Park'],
+'movies': ["Hitchhiker's Guide to the Galaxy", "The Hobbit"]
+}
+print('My favorite books are', fav_dict['books'])
+
+test_tuple = ['baking', 'traveling', 'skating']
+print('A hobby of mine includes', test_tuple[0])
