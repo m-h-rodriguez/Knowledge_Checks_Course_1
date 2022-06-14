3127490b2781d18700fdeb163c742b3300bd97e5
m-h-rodriguez <michellehope100@gmail.com>
2022-06-14 19:57:40 -0400

    adding import os function

--- knowledgecheck_2.py
+++ knowledgecheck_2.py
@@ -1,17 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
+import os
 
-df = pd.read_csv(
-    "C:\\Users\\miche\\OneDrive\\Desktop\\Code Louisville\\knowledge checks\\data_1_checks\\assets\\SalaryData.csv", encoding='unicode_escape')
 
-# print(df.head())
-# print(df.tail())
+df = pd.read_csv(os.path.join(os.path.dirname(
+    __file__), 'assets', 'SalaryData'.csv))
+
+# df = pd.read_csv(
+#     "C:\\Users\\miche\\OneDrive\\Desktop\\Code Louisville\\knowledge checks\\data_1_checks\\assets\\SalaryData.csv", encoding='unicode_escape')
+
+print(df.head())
+print(df.tail())
+
+
+def year(salary): return df['CalYear']
+
+
+year(2020)
 
 
 dept = df['Department'].unique()
-salary = df.groupby(['Department'])['Annual_Rate'].sum()
+salary = df.groupby(year, ['Department'])['Annual_Rate'].sum()
 
-print(dept)
 
 plt.plot(dept, salary, 'o')
 plt.figure(figsize=(20, 5))
